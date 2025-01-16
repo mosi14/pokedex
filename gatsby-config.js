356f3bf91936c1428e9ae9080519691aabecfb93
mosio14 <mostafa.asadollahy@gmail.com>
2025-01-16 15:49:54 +0100

    add custom plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,28 @@
 module.exports = {
   siteMetadata: {
     title: `pokedex`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      resolve: require.resolve("./plugins/gatsby-source-pokemon"),
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
