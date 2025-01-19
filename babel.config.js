22762507ccb0a24373a94ed826d23b47db8de7bc
mosio14 <mostafa.asadollahy@gmail.com>
2025-01-19 14:25:14 +0100

    delete some small thigs

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
-    ["@babel/plugin-proposal-private-methods", { loose: true }],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+    ["@babel/plugin-proposal-private-methods"],
+    ["@babel/plugin-proposal-private-property-in-object"],
   ],
 };
